b6d437e702ff472074aa3c0ed3cac06ecf6544f6
Justin Lee <jstnlee365@gmail.com>
2021-10-14 22:56:24 -0400

    add notification time

--- options.js
+++ options.js
@@ -1,14 +1,17 @@
 const nameInput = document.getElementById('name-input');
+const notificationTimeInput = document.getElementById('notification-time-input');
 const saveBtn = document.getElementById('save-btn');
 
 saveBtn.addEventListener('click', () => {
   const name = nameInput.value;
-  chrome.storage.sync.set({ name }, () => {
+  const notificationTime = notificationTimeInput.value;
+  chrome.storage.sync.set({ name, notificationTime }, () => {
     console.log(`Name is set to ${name}`);
   });
 });
 
-chrome.storage.sync.get(["name", "test"], (result) => {
+chrome.storage.sync.get(["name", "notificationTime"], (result) => {
   console.log(result);
   nameInput.value = result.name;
+  notificationTimeInput.value = result.notificationTime ?? 1000;
 });
